faaaf9f0a76542a38d406a57115d95fb07fc9e40
Patrick Steffes <psteffes@umich.edu>
2021-12-08 13:55:10 -0500

    add trailing slash to urls

--- ui/urls.py
+++ ui/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from ui.views import AuthenticatedHomeView, StudentView, EmployeeView, UnauthorizedView
 
 urlpatterns = [
-    path('authenticated', AuthenticatedHomeView.as_view(), name='authenticated'),
-    path('student', StudentView.as_view(), name='student'),
-    path('employee', EmployeeView.as_view(), name='employee'),
-    path('unauthorized', UnauthorizedView.as_view(), name='unauthorized')
+    path('authenticated/', AuthenticatedHomeView.as_view(), name='authenticated'),
+    path('student/', StudentView.as_view(), name='student'),
+    path('employee/', EmployeeView.as_view(), name='employee'),
+    path('unauthorized/', UnauthorizedView.as_view(), name='unauthorized')
 ]
